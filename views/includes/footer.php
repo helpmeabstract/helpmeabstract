f8f7df6390565cb8c00dd9e92b258caf772695a8
Kayla Daniels <kayladnls@gmail.com>
2015-07-15 19:43:09 -0400

    amateur hour over here.

--- views/includes/footer.php
+++ views/includes/footer.php
@@ -1,7 +1,7 @@
 <footer id="footer" class="container">
     <div id="copyright">
         <ul class="menu">
-            <li>&copy; Untitled. All rights reserved</li><li>Design: <a href="http://html5up.net">HTML5 UP</a></li>
+            <li>&copy; 2015 HelpMeAbstract. All rights reserved</li><li>Design: <a href="http://html5up.net">HTML5 UP</a></li>
         </ul>
     </div>
 </footer>
