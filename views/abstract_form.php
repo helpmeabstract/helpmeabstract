55c95175ed8ea3625c457e94dbabc739fd7cc9a7
Kayla Daniels <kayladnls@gmail.com>
2015-07-15 19:33:05 -0400

    update disclaimer

--- views/abstract_form.php
+++ views/abstract_form.php
@@ -1,4 +1,5 @@
-<div id="form" style="background:#ff4486; width: 97%; padding-bottom:100px;  margin-top:20px;margin-left: 40px;border-radius: 10px;">
+<div id="form"
+     style="background:#ff4486; width: 97%; padding-bottom:100px;  margin-top:20px;margin-left: 40px;border-radius: 10px;">
     <div class="8u -2u important(collapse)" style="background:#ff4486">
         <!-- Content -->
         <div id="content">
@@ -6,6 +7,7 @@
             <h3><i class="fa fa-exclamation-triangle"></i>{{error}}</h3>
             {% endif %}
             <h2 style="color:white">Submit Abstract</h2>
+
             <form action="/submitAbstract" method="post">
                 <label for="name">Name</label>
                 <input type="text" name="name">
@@ -13,6 +15,13 @@
                 <input type="text" name="email">
                 <label for="link">Gist Link</label>
                 <input type="text" name="link">
+
+                <small style="color:white">
+                    <i>
+                        * Please submit your abstracts in Markdown format or ensure that lines are less than 80 chars long
+                    </i>
+                </small>
+                <br>
                 <br>
                 <div class="row">
                     <div class="4u -5u">
@@ -20,6 +29,5 @@
                     </div>
                 </div>
             </form>
-            <small>Please submit your abstracts in Markdown format or ensure that lines are less than 80 chars long</small>
         </div>
     </div>
