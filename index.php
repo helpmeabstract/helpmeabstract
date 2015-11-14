46f6a93e24d198978bc87342a19a01a386d38ac6
Katie McLaughlin <katie@glasnt.com>
2015-11-14 14:15:10 +1100

    Add helper to serve local static files

--- index.php
+++ index.php
@@ -10,6 +10,15 @@ use GuzzleHttp\Client;
 use Spot\Config;
 use Spot\Locator;
 
+// Decline static file requests back to the PHP built-in webserver
+if (php_sapi_name() === 'cli-server') {
+    $path = realpath(__DIR__ . parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH));
+    if (__FILE__ !== $path && is_file($path)) {
+        return false;
+    }
+    unset($path);
+}
+
 require_once(__DIR__ . '/vendor/autoload.php');
 Dotenv::load(__DIR__);
 
