4091efa1611dcc20efa08d66c85e7d6d36ce1475
Graham Daniels <greydnls@gmail.com>
2016-11-18 10:45:27 -0500

    Fix twitter handle

--- views/abstract_thankyou.php
+++ views/abstract_thankyou.php
@@ -16,7 +16,7 @@
                         <h3>We've sent your abstract off for review!</h3>
                         <p>You should receive feedback on your abstract within a few days. Our volunteers are notified of new submissions and review them as time permits.</p>
                         <p>In the rare case you haven't received feedback after three days, please submit your abstract again.  We hope this doesn't happen often, but at busy times abstracts can slip by unreviewed.</p>
-                        <p>If you want to be notified about comments to your gists, sign up at <a href="https://giscus.co/">Giscus</a>.  If you have tagged someone in a comment, and they haven't responded, you might try twitter.  If you have any problems, please contact <a href="https://twitter.com/kayladnls">Kayla Daniels via Twitter</a></a></p>
+                        <p>If you want to be notified about comments to your gists, sign up at <a href="https://giscus.co/">Giscus</a>.  If you have tagged someone in a comment, and they haven't responded, you might try twitter.  If you have any problems, please contact <a href="https://twitter.com/helpmeabstract">HelpMeAbstract via Twitter</a></a></p>
                         <h3 class="call-to-action">Wouldn't it be great if our industry was a safe place for all? Please consider supporting <a href="http://codemanifesto.com/">The Code Manifesto</a>.</h3>
                     </div>
                 </div>
