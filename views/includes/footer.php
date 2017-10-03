7fe3a4196947b91aa46d3c21130c86fab1d52eb5
Steve Winter <steve@msdev.co.uk>
2017-10-03 10:49:21 +0300

    Updating footer to include current year in copyright statement and updated github repo

--- views/includes/footer.php
+++ views/includes/footer.php
@@ -1,9 +1,9 @@
 <footer id="footer" class="container">
     <div id="copyright">
         <ul class="menu">
-            <li>&copy; 2015 HelpMeAbstract. All rights reserved</li>
+            <li>&copy; 2015 - 2017 HelpMeAbstract. All rights reserved</li>
             <li>Design: <a href="http://html5up.net">HTML5 UP</a></li>
-            <li><a href="https://github.com/greydnls/helpmeabstract">Improve this website</a></li>
+            <li><a href="https://github.com/helpmeabstract/helpmeabstract">Improve this website</a></li>
         </ul>
     </div>
 </footer>
