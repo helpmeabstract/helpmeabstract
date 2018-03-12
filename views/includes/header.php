c1c9823ae82da48b5471605d82112cb945099f2c
Nicolas Steenhout <vavroom@users.noreply.github.com>
2018-03-12 12:55:33 -0400

    Update header.php
    Adding lang attribute to the <html> tag to allow screen readers to parse and announce the page in the correct voice/language.

--- views/includes/header.php
+++ views/includes/header.php
@@ -4,7 +4,7 @@ Verti by HTML5 UP
 	html5up.net | @n33co
 	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
 -->
-<html>
+<html lang="en">
 <head>
     <title>HelpMeAbstract</title>
     <meta http-equiv="content-type" content="text/html; charset=utf-8" />
