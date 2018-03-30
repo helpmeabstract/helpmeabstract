e042828f3836f71a1dc7b502b496d32818939208
Graham Daniels <greydnls@gmail.com>
2018-03-30 10:38:50 -0400

    Update Footer

--- views/includes/footer.php
+++ views/includes/footer.php
@@ -1,7 +1,8 @@
 <footer id="footer" class="container">
     <div id="copyright">
         <ul class="menu">
-            <li>&copy; 2015 - 2017 HelpMeAbstract. All rights reserved</li>
+            <li>&copy; 2015 - 2018 HelpMeAbstract. All rights reserved</li>
+            <li> Built with ♥️ in Brooklyn</li>
             <li>Design: <a href="http://html5up.net">HTML5 UP</a></li>
             <li><a href="https://github.com/helpmeabstract/helpmeabstract">Improve this website</a></li>
         </ul>
